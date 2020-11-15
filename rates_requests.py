8ed143edb0b027590b279491a986c7adca57066a
applefighting <445694126@qq.com>
2020-11-15 18:35:03 +0800

    add note

--- rates_requests.py
+++ rates_requests.py
@@ -4,44 +4,30 @@ import json
 url = 'https://api.huobi.pro/general/exchange_rate/list'
 r = requests.get(url)
 
+#显示接口请求状态码
 print('status code:', r.status_code)
 
+#将API相应存储在一个变量中
 response_dict = r.json()
 
-# print ('Total rates:', response_dict['total_count'])
-
+#将汇率相关的汇率的数据提取出来
 repo_dicts = response_dict['data']
+
+#分别创建一个币对和汇率的空列表
 coinpairs = []
 pairrates = []
+
+#将字典中的币对和汇率值分别添加到列表中
 for pairs in repo_dicts:
     p = pairs['name']
     coinpairs.append(p)
     r = pairs['rate']
     pairrates.append(r)
-    
+
+#将两个列表合并为一个字典，币对为键汇率为值    
 rate_dict = dict(zip(coinpairs,pairrates))
 
-# basecoin = input('请输入基础货币:')
-# quotecoin = input ('请输入计价货币:')
-
-# get_pair = basecoin + "_" + quotecoin
-
-# if get_pair in coinpairs:
-#     print(get_pair,"Rate:",rate_dict[get_pair])
-# else:
-#     print('无该币对')
-
+#创建一个json文件，将创建的汇率字典存储下来
 filename = 'huobi_rates.json'
 with open (filename,'w') as f:
     json.dump(rate_dict,f)
-
-# print('usd_mdl:',rate_dict['usd_mdl'])
-# print('Total rate:', len(repo_dicts))
-# repo_dict = repo_dicts[0]
-# print ('\nKeys:', len(repo_dict))
-
-# for key in sorted(repo_dict.keys()):
-#     print(key)
-
-# print ('Pairs:', repo_dict['name'])
-# print ('Rate:', repo_dict['rate'])
